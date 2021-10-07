4c7a838e611574ea5cae7dfcf7936100042d6837
Sanvi-Agarwal <85011956+Sanvi-Agarwal@users.noreply.github.com>
2021-10-07 16:34:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,3 +20,18 @@ classifer = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/
 function modelLoaded() {
     console.log("Model Loaded!");
 }
+
+function check() {
+    img = document.getElementById('captured_image');
+    classifer.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3) * 100 + "%";
+    }
+}
